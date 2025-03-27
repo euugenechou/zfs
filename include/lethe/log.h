a26066e55b5b87f24f1c13b2f6214a7b34d8ed06
Eugene Chou <euchou@ucsc.edu>
2025-03-27 02:33:28 +0000

    fix lethe locking-via-macro when DEBUG is enabled

--- include/lethe/log.h
+++ include/lethe/log.h
@@ -16,24 +16,22 @@
     #define lethe_warn(...) lethe_log(pr_warn, "warn", __VA_ARGS__)
     #define lethe_error(...) lethe_log(pr_error, "error", __VA_ARGS__)
     #define lethe_cont(...) pr_cont(__VA_ARGS__)
-
-    #define lethe_rw_enter(lock, type) \
-        do { \
-            lethe_info("locking %p from thread %p\n", (void *)lock, (void *)current); \
-            rw_enter(lock, type); \
-        } while (0)
-
-    #define lethe_rw_exit(lock) \
-        do { \
-            lethe_info("unlocking %p from thread %p\n", (void *)lock, (void *)current); \
-            rw_exit(lock); \
-        } while (0)
 #else
     #define lethe_log(...) ((void)0)
     #define lethe_info(...) ((void)0)
     #define lethe_warn(...) ((void)0)
     #define lethe_error(...) ((void)0)
     #define lethe_cont(...) ((void)0)
-    #define lethe_rw_enter(...) ((void)0)
-    #define lethe_rw_exit(...) ((void)0)
 #endif
+
+#define lethe_rw_enter(lock, type) \
+    do { \
+        lethe_info("locking %p from thread %p\n", (void *)lock, (void *)current); \
+        rw_enter(lock, type); \
+    } while (0)
+
+#define lethe_rw_exit(lock) \
+    do { \
+        lethe_info("unlocking %p from thread %p\n", (void *)lock, (void *)current); \
+        rw_exit(lock); \
+    } while (0)
